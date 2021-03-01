0d230c2a27d5779a3264e2c613b5a2a83d2c56c3
Andre Hogenkamp <tikaltikal@gmail.com>
2021-03-01 22:33:24 +0100

    added rxjs and chart library

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Koa = require('koa');
+const serve = require('koa-static');
+const app = new Koa();
+const root = '/';
+const opts = {};
+
+// logger
+
+app.use(serve(root, opts));
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.listen(8000);
