2501533494079264922d0e00db11f53a7e85f1ef
Andre Hogenkamp <tikaltikal@gmail.com>
2021-04-03 22:44:05 +0200

    Example graphs for cold observer

--- src/hot-vs-cold.js
+++ src/hot-vs-cold.js
@@ -1,5 +1,5 @@
 import { of, interval, zip } from "rxjs";
-import { map, delay, filter } from "rxjs/operators";
+import { map, delay, filter, skip, publish } from "rxjs/operators";
 import {chart1, chart2, chart3} from './linechart';
 
 const interval$ = interval(100);
@@ -14,18 +14,22 @@ delayedrange$.subscribe(data => {
 chart1.showSeries('Desktops');
 
 console.log(`type of chart: ${chart1}`);
-delayedrange$.subscribe(data => {
-    console.log(`appending data to chart 2: ${data}`);
-    chart2.appendData([{
-        data: [data]
-      }]);
-});
+delayedrange$
+    .pipe(delay(1000))
+    .subscribe(data => {
+        console.log(`appending data to chart 2: ${data}`);
+        chart2.appendData([{
+            data: [data]
+        }]);
+    });
 chart2.showSeries('Desktops');
 
-delayedrange$.subscribe(data => {
-    console.log(`appending data to chart 3: ${data}`);
-    chart3.appendData([{
-        data: [data]
-      }]);
-});
+delayedrange$
+    .pipe(delay(2000))
+    .subscribe(data => {
+        console.log(`appending data to chart 3: ${data}`);
+        chart3.appendData([{
+            data: [data]
+        }]);
+    });
 chart3.showSeries('Desktops');
--- src/linechart.js
+++ src/linechart.js
@@ -7,227 +7,145 @@
 
 import ApexCharts from 'apexcharts';
 
+const chartHeight = 350;
+const series = [{
+    name: "Desktops",
+    data: []
+}];
+const gridDefinition = {
+    row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+    },
+};
+const chartDefinition = {
+    height: chartHeight,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+};
+const annotations = {
+    xaxis: [
+      {
+        x: 'Apr',
+        borderColor: '#775DD0',
+        label: {
+          style: {
+            color: '#7c3aed',
+          },
+          text: 'X-axis annotation - Apr'
+        }
+      },
+      {
+        x: 'Aug',
+        borderColor: '#775DD0',
+        label: {
+          style: {
+            color: '#7c3ced',
+          },
+          text: 'X-axis annotation - Aug'
+        }
+      },
+      {
+        x: 'Dec',
+        borderColor: '#775DD0',
+        label: {
+          style: {
+            color: '#7c3ced',
+          },
+          text: 'X-axis annotation - Dec'
+        }
+      }
+    ]
+};
+
 var options = {
-    series: [{
-      name: "Desktops",
-      data: []
-  }],
-    chart: {
-    height: 350,
-    type: 'line',
-    zoom: {
-      enabled: false
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  colors: ["#8B5CF6"],
-  stroke: {
-    curve: 'straight'
-  },
-  title: {
-    text: 'Product Trends by Month',
-    align: 'left'
-  },
-  grid: {
-    row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
+    series: series,
+    chart: chartDefinition,
+    dataLabels: {
+        enabled: false
     },
-  },
-  xaxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  },
-  annotations: {
-    xaxis: [
-      {
-        x: 'Apr',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3aed',
-          },
-          text: 'X-axis annotation - Apr'
-        }
-      },
-      {
-        x: 'Aug',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Aug'
-        }
-      },
-      {
-        x: 'Dec',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Dec'
-        }
-      }
-    ]
-  }
-  };
-
-  var chart1 = new ApexCharts(document.querySelector("#chart1"), options);
-  chart1.id = 'chart-1';
-  chart1.group = 'social1',
-  chart1.render();
-  
-  var options2 = {
-    series: [{
-      name: "Desktops",
-      data: []
-  }],
-    chart: {
-    height: 350,
-    type: 'line',
-    zoom: {
-      enabled: false
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  colors: ["#8B5CF6"],
-  stroke: {
-    curve: 'straight'
-  },
-  title: {
-    text: 'Product Trends by Month',
-    align: 'left'
-  },
-  grid: {
-    row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
+    colors: ["#8B5CF6"],
+    stroke: {
+        curve: 'straight'
     },
-  },
-  xaxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  },
-  annotations: {
-    xaxis: [
-      {
-        x: 'Apr',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3aed',
-          },
-          text: 'X-axis annotation - Apr'
-        }
-      },
-      {
-        x: 'Aug',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Aug'
-        }
-      },
-      {
-        x: 'Dec',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Dec'
-        }
-      }
-    ]
-  }
-  };
-
-  var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
-  chart2.id = 'chart-2';
-  chart2.group = 'social2',
-  chart2.render();
-
-  var options3 = {
-    series: [{
-      name: "Desktops",
-      data: []
-  }],
-    chart: {
-    height: 350,
-    type: 'line',
-    zoom: {
-      enabled: false
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  colors: ["#8B5CF6"],
-  stroke: {
-    curve: 'straight'
-  },
-  title: {
-    text: 'Product Trends by Month',
-    align: 'left'
-  },
-  grid: {
-    row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
+    title: {
+        text: 'Product Trends by Month',
+        align: 'left'
     },
-  },
-  xaxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  },
-  annotations: {
-    xaxis: [
-      {
-        x: 'Apr',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3aed',
-          },
-          text: 'X-axis annotation - Apr'
-        }
-      },
-      {
-        x: 'Aug',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Aug'
-        }
-      },
-      {
-        x: 'Dec',
-        borderColor: '#775DD0',
-        label: {
-          style: {
-            color: '#7c3ced',
-          },
-          text: 'X-axis annotation - Dec'
-        }
-      }
-    ]
-  }
-  };
+    grid: gridDefinition,
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+    annotations: annotations
+};
 
-  var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
-  chart3.id = 'chart-3';
-  chart3.group = 'social3',
-  chart3.render();
+var chart1 = new ApexCharts(document.querySelector("#chart1"), options);
+chart1.id = 'chart-1';
+chart1.group = 'social1',
+chart1.render();
 
-  module.exports = {
+var options2 = {
+    series: [{
+        name: "Desktops",
+        data: []
+    }],
+    chart: chartDefinition,
+    dataLabels: {
+        enabled: false
+    },
+    colors: ["#8B5CF6"],
+    stroke: {
+        curve: 'straight'
+    },
+    title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+    },
+    grid: gridDefinition,
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+    annotations: annotations
+};
+
+var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+chart2.id = 'chart-2';
+chart2.group = 'social2',
+chart2.render();
+
+var options3 = {
+    series: [{
+        name: "Desktops",
+        data: []
+    }],
+    chart: chartDefinition,
+    dataLabels: {
+        enabled: false
+    },
+    colors: ["#8B5CF6"],
+    stroke: {
+        curve: 'straight'
+    },
+    title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+    },
+    grid: gridDefinition,
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+    annotations: annotations
+};
+
+var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+chart3.id = 'chart-3';
+chart3.group = 'social3',
+chart3.render();
+
+module.exports = {
     chart1: chart1,
     chart2: chart2,
     chart3: chart3
-  }
+}
